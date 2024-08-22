24d2d8e69db70ceab888cf392b0f7a10ed3cc55b
elena_soboleva <elenasoboleva2003@yandex.ru>
2024-08-22 22:57:51 +0300

    changed k8s

--- gradio/frontend.py
+++ gradio/frontend.py
@@ -8,16 +8,21 @@ def send_request(file):
     df = pd.read_csv(file.name)
 
     # Convert DataFrame to JSON (this depends on your API requirements)
-    data_json = df.to_json(orient="records")
+    data_json = df.to_json(orient='split')
+    payload = {'data': data_json}
 
     # Example API endpoint (replace with your actual endpoint)
-    url = "https://api.example.com/endpoint"
+    url = "http://api:8000/best_model"
 
     # Send POST request to the API (you may need to adjust headers and data format)
-    response = requests.post(url, json={"data": data_json})
+    response = requests.post(url, json=payload)
+
+    res = pd.read_json(response.json()['pred'], orient='split')
+    res.columns = ['Prediction']
+    pred_res = pd.concat([df, res], axis=1)
 
     # Return the response from the API
-    return response.json()
+    return pred_res
 
 # Create a Gradio interface
 with gr.Blocks() as demo:
